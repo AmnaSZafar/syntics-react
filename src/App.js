ac7c495ad034fa95d214783dde33355954ffc591
AmnaSZafar <amna.s.zafar@gmail.com>
2021-10-24 20:02:29 +0500

    only-front-end

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
+import Seminar from './pages/Seminar';
+import Apply from './pages/Apply';
+// import Login from './pages/Login';
+// import Register from './pages/Register';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <Router>
+        <Header />
+
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/apply' component={Apply} />
+          <Route exact path='/seminar' component={Seminar} />
+         
+        </Switch>
+
+        <Footer />
+
+      </Router>
+
     </div>
+    
   );
 }
 
